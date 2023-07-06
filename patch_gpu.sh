8d7c1a98b9bead9c03fe8317fa55b5facee345c0
Dan Rahmel <drahmel7@gmail.com>
2023-07-06 09:03:34 -0700

    DJR:Added startup scripts

---
+++ patch_gpu.sh
@@ -0,0 +1,4 @@
+cd /home/ubuntu/.local/lib/python3.8/site-packages/bitsandbytes
+mv /home/ubuntu/.local/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so ~
+cp  libbitsandbytes_cuda117.so libbitsandbytes_cpu.so
+
---
+++ start.sh
@@ -0,0 +1,6 @@
+python download-model.py Facebook/opt-350m
+python download-model.py PygmalionAI/pygmalion-1.3b
+python download-model.py PygmalionAI/pygmalion-2.7b
+python download-model.py PygmalionAI/pygmalion-6b
+
+python server.py --share --chat --api
